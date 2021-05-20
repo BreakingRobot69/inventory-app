2b9b0dc4554ff51834b40df1f92f878907829131
Tony Briet <tony@folhomee.fr>
2021-05-20 17:35:38 +0200

    feat(touchable): add light haptic on pressable

--- src/components/atoms/Touchable/index.js
+++ src/components/atoms/Touchable/index.js
@@ -1,15 +1,25 @@
 import React from 'react'
+import * as Haptics from 'expo-haptics'
 import { Pressable } from 'react-native'
 
-const Touchable = ({ children, ...props }) => {
+const Touchable = ({ children, onPress, ...props }) => {
+  const handlePress = event => {
+    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light)
+    return onPress()
+  }
   return (
-    <Pressable {...props}>
+    <Pressable onPress={handlePress} {...props}>
       {children}
     </Pressable>
   )
 }
+
 Touchable.propTypes = {
   ...Pressable.PropTypes
+}
+
+Touchable.defaultProps = {
+  onPress: () => {}
 }
 
 export default Touchable
