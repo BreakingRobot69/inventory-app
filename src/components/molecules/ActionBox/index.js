22e2e5563fcd5c0aeade2664bda5e3abd8192010
Tony Briet <tony@folhomee.fr>
2021-05-20 17:35:58 +0200

    feat(action-box): add an action box inherited from touchable

---
+++ src/components/molecules/ActionBox/index.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import styled, { useTheme } from 'styled-components/native'
+import { get, isEmpty } from 'lodash'
+
+import Touchable from '../../atoms/Touchable'
+import { Ionicons } from '@expo/vector-icons'
+import Text from '../../atoms/Text'
+
+const Box = styled(Touchable)`
+  width: 128px;
+  height: 128px;
+  border-radius: 5px;
+  align-items: center;
+  border-width: 1.5px;
+  border-style: dashed;
+  align-self: ${({ align = 'auto' }) => align};
+  justify-content: center;
+  border-color: ${({ theme, borderColor }) => get(theme, `colors.${borderColor}`, borderColor)};
+`
+
+const ActionBox = ({ text, align, onPress, icon, iconSize, iconColor, borderColor, textProps }) => {
+  const theme = useTheme()
+
+  return (
+    <Box
+      align={align}
+      onPress={onPress}
+      borderColor={borderColor}>
+      {!isEmpty(icon) && (
+        <Ionicons name={icon} size={iconSize} color={get(theme, `colors.${iconColor}`)} />
+      )}
+      <Text {...textProps}>
+        {text}
+      </Text>
+    </Box>
+  )
+}
+
+ActionBox.defaultProps = {
+  align: 'auto',
+  onPress: () => {},
+  text: 'Placeholder',
+  iconSize: 32,
+  iconColor: 'blue',
+  borderColor: 'blueyGrey'
+}
+
+export default ActionBox
