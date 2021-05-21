cd0d5b9093fd61288694ad213547725d7bc9908a
Tony Briet <tony@folhomee.fr>
2021-05-21 13:47:24 +0200

    feat(screen-lock): add screen-lock

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Provider } from 'react-redux'
 import { registerRootComponent } from 'expo'
 import { enableScreens } from 'react-native-screens'
@@ -6,6 +6,7 @@ import { ThemeProvider } from 'styled-components/native'
 import { AppearanceProvider } from 'react-native-appearance'
 import { PersistGate } from 'redux-persist/integration/react'
 import { ActionSheetProvider } from '@expo/react-native-action-sheet'
+import * as ScreenOrientation from 'expo-screen-orientation';
 import 'react-native-gesture-handler'
 
 import theme from './config/theme'
@@ -15,6 +16,12 @@ import { store, persistor } from './redux/bootstrapStore'
 enableScreens()
 
 const App = () => {
+  useEffect(() => {
+    (async () => {
+      await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT_UP)
+    })()
+  })
+
   return (
     <Provider store={store}>
       <PersistGate
